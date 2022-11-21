24e596f2e4aa059ee23027013295927d04323a37
Samuel <samuelsantanadasilva8@gmail.com>
2022-11-21 18:46:32 -0300

    Adicioandno novos estudos reactJs

--- react-mateusBelloti/meu/src/App.js
+++ react-mateusBelloti/meu/src/App.js
@@ -1,9 +1,71 @@
+import { useState } from 'react';
 import './App.css';
-import OlaMundo from './components/olaMundo'
+import Evento from './components/evento'
+import Form from './components/form';
+import Condicional from './components/condicional';
+import OutraLista from './components/outraLista';
+import SeuNome from './components/seuNome';
+import Saudacao from './components/saudacao';
+
+
+function App() {
+  const time = 'chelsea'
+  const name = 'samuel'
+  const novoNome = name.toUpperCase()
+
+  function soma(a, b){
+    return a + b
+  }
+
+ //Essa aqui é do arquivo OutraLista
+  const minhaLista = ['angular', 'futebol', 'basquete']
+ 
+  //Esse aqui é do SeuNome/Saudacao
+  const [nome, setNome] = useState()
+
+ 
+  return (
+    <div className="App">
+     <h1>Primeiro projeto </h1>
+      <Evento />
+      <br>
+      </br>
+      <hr></hr>
+      <Form />
+      <br>
+      </br>
+      <hr></hr>
+      <Condicional />
+      <br></br>
+      <hr></hr>
+      <OutraLista lista={minhaLista} />
+      <br></br>
+      <hr></hr>
+      <SeuNome setNome={setNome} />
+      <Saudacao nome={nome} />
+    </div>
+
+  );
+}
+
+export default App;
+
+/* Primeiros arquivos de treino que sao colocados dentro da div
+ *  <p>Seja bem vindo {novoNome}</p>
+     <p>Multiplicação: {2*12}</p>
+     <p>Soma: {soma(10,20)}</p>
+     <OlaMundo />
+     <Teste />
+     <PropsUm time={time} />
+     <PropsDois pessoa="ronaldo" idade="18" pais="brasil" foto="https://via.placeholder.com/150"  />
+     <Fragmento />
+     ** Arquivos que ficam la em cima para fazer importaçao
+     import OlaMundo from './components/olaMundo'
 import Teste from './components/componenteDois'
 import PropsUm from './components/propsUm';
 import PropsDois from './components/propsDois';
 import Fragmento from "./components/fragmentos";
+ */ 
 
 
 /*
@@ -15,36 +77,10 @@ import Fragmento from "./components/fragmentos";
  * Usar muito o return 
  * No jsx se chama as situaçoes e nao se cria dentro delas,ou seja,cria em outros lugares e depois so as chama dentro do jsx
  */
-function App() {
-  const time = 'chelsea'
-  const name = 'samuel'
-  const novoNome = name.toUpperCase()
 
-  function soma(a, b){
-    return a + b
-  }
-
-  /*
+ /*
    * A primeira letra sempre tem que ser maiuscula para poder iniciar e tem que ser no aqui na exportaçao e na importaçao
    * Sempre dentro de uma div
    * No propsUm chamou a variavel do arquivo propsUm e trouxe pra esse so fez uma conexao das variaveis para mostrar o valor
    * Na props cria la no outro arquivo o formato e so chama aqui por aqui para aparecer na tela
    */
-
-  return (
-    <div className="App">
-     <h1>Primeiro projeto </h1>
-     <p>Seja bem vindo {novoNome}</p>
-     <p>Multiplicação: {2*12}</p>
-     <p>Soma: {soma(10,20)}</p>
-     <OlaMundo />
-     <Teste />
-     <PropsUm time={time} />
-     <PropsDois pessoa="ronaldo" idade="18" pais="brasil" foto="https://via.placeholder.com/150"  />
-     <Fragmento />
-    </div>
-
-  );
-}
-
-export default App;
